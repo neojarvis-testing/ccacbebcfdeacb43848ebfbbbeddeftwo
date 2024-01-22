ab83922bc564df389b871e3f3f98021a61cbacde
neojarvis <techadmin@iamneo.ai>
2024-01-22 10:36:51 +0000

    Submit-project-save-commit-1/22/2024--4:06:45-PM

--- Day1Q1/Day1Q1/settings.py
+++ Day1Q1/Day1Q1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4swy8^06y5#$viap-$e0&&hhvbp2ovv54tboy#g++b0$i!mo%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
